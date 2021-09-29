91be579e9e821881ccc14f113d4e8798fd87903b
onedou <onedou@126.com>
2021-09-29 09:18:20 +0800

    updated UpgradeMod.sh

--- bin/UpgradeMod.sh
+++ bin/UpgradeMod.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+machine_username=$(whoami)
+resource_path=/Users/$machine_username/Library/Containers/com.tatfook.paracraftmac/Data/Documents/Paracraft/files
+mod_root=$resource_path/dev
+
+update_mod()
+{
+    mod_name="$1"
+
+    echo $mod_name
+    pushd $mod_root/$mod_name
+    git pull --rebase
+    popd
+}
+
+update_mod WorldShare
+update_mod ExplorerApp
