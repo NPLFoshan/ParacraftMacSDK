0ad05f872ed44bb5722130dafe67b4d45bb1812f
onedou <onedou@126.com>
2023-02-09 11:32:23 +0800

    updated.

--- bin/CopyScriptCI.sh
+++ bin/CopyScriptCI.sh
@@ -8,8 +8,9 @@ application_path=/Applications/Paracraft.app
 dest_path=/Volumes/CODE/NPLRuntimeCI/NPLRuntime/Platform/iOS/assets # change to your project folder
 paracraft_build_in_mod_path=/Volumes/CODE/ParacraftBuildinMod
 
-pushd $dev_path/trunk
-zip -r $dev_path/main.zip ./ -x "./.git/*"
+pushd /Volumes/CODE/trunkCI
+git pull origin dev --rebase
+zip -r $dev_path/mainCI.zip ./ -x "./.git/*"
 popd
 
 pushd $files_path
@@ -22,27 +23,27 @@ if [ -f "$dest_path/main.pkg" ]; then
 	rm $dest_path/main.pkg
 fi
 
-cat >./create_pkg.lua <<EOL
+cat >./create_pkg_ci.lua <<EOL
 NPL.load("(gl)script/ide/commonlib.lua"); 
 
 local filesPath = "${files_path}"
 local destPath = "${dest_path}"
-local originFile = "${dev_path}/main.zip"
-local encryptFile = "${dev_path}/main.pkg"
+local originFile = "${dev_path}/mainCI.zip"
+local encryptFile = "${dev_path}/mainCI.pkg"
 
 ParaAsset.GeneratePkgFile(originFile, encryptFile)
 -- ParaIO.CopyFile(encryptFile, destPath .. "/main.pkg", true)
 
 ParaIO.DeleteFile(originFile)
 --ParaIO.DeleteFile(encryptFile)
-ParaIO.DeleteFile(filesPath .. "/create_pkg.lua")
+ParaIO.DeleteFile(filesPath .. "/create_pkg_ci.lua")
 ParaIO.DeleteFile(filesPath .. "/config.txt")
 
 ParaGlobal.ExitApp()
 EOL
 
 cat >./config.txt <<EOL
-cmdline=noupdate="true" bootstrapper="create_pkg.lua" 
+cmdline=noupdate="true" bootstrapper="create_pkg_ci.lua" 
 EOL
 
 open -a $application_path
@@ -55,18 +56,18 @@ if [ -f "$files_path/config.txt.1" ]; then
 	mv $files_path/config.txt.1 $files_path/config.txt
 fi
 
-cp $dev_path/main.pkg $dest_path/
-rm $dev_path/main.pkg
+cp $dev_path/mainCI.pkg $dest_path/main.pkg
+rm $dev_path/mainCI.pkg
 rm -r $dest_path/npl_packages/ParacraftBuildinMod.zip
 
-rm -r $paracraft_build_in_mod_path/npl_packages/WorldShare/Mod
-rm -r $paracraft_build_in_mod_path/npl_packages/ExplorerApp/Mod
+# rm -r $paracraft_build_in_mod_path/npl_packages/WorldShare/Mod
+# rm -r $paracraft_build_in_mod_path/npl_packages/ExplorerApp/Mod
 
-cp -r $dev_path/WorldShare/Mod $paracraft_build_in_mod_path/npl_packages/WorldShare
-cp -r $dev_path/ExplorerApp/Mod $paracraft_build_in_mod_path/npl_packages/ExplorerApp
+# cp -r $dev_path/WorldShare/Mod $paracraft_build_in_mod_path/npl_packages/WorldShare
+# cp -r $dev_path/ExplorerApp/Mod $paracraft_build_in_mod_path/npl_packages/ExplorerApp
 
 pushd $paracraft_build_in_mod_path
-bash ./build_without_update.sh
+bash ./build.sh
 popd
 
 cp $paracraft_build_in_mod_path/ParacraftBuildinMod.zip $dest_path/npl_packages/
